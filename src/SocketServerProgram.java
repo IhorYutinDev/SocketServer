6567d6ef1fa2a6af5c8cdf25e8cbaa6e5e61749f
IhorYutinDev <ihor.yutin.dev@gmail.com>
2020-11-12 09:37:50 +0200

    Commit 12112020

--- src/SocketServerProgram.java
+++ src/SocketServerProgram.java
@@ -15,41 +15,49 @@ public class SocketServerProgram implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Server have got a new connection!");
-        readClientMessage();
+
         try {
-            Thread.sleep(3500);
-        } catch (InterruptedException e) {
+            System.out.println("Server have got a new connection!");
+            readClientMessage();
+            try {
+                Thread.sleep(3500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Sending massage to client...");
+            writeMessageToClient();
+        } catch (Exception e) {
             e.printStackTrace();
+        }finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        System.out.println("Sending massage to client...");
-        writeMessageToClient();
-        try {
-            serverSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+
     }
 
-    private void readClientMessage() {
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-        try {
-            clientMessage = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
+        private void readClientMessage () {
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            try {
+                clientMessage = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Server already has got message: " + clientMessage);
         }
-        System.out.println("Server already has got message: " + clientMessage);
-    }
 
-    private void writeMessageToClient() {
-        PrintWriter out = new PrintWriter(outputStream, true);
-        String messageOut;
-        if (clientMessage.equals("")) {
-            messageOut = "Your message is empty, is it ok?";
-        } else {
-            messageOut = "Hi, from server! Server has got your message: " + clientMessage;
+        private void writeMessageToClient () {
+            PrintWriter out = new PrintWriter(outputStream, true);
+            String messageOut;
+            if (clientMessage.equals("")) {
+                messageOut = "Your message is empty, is it ok?";
+            } else {
+                messageOut = "Hi, from server! Server has got your message: " + clientMessage;
+            }
+            out.println(messageOut);
+            out.flush();
         }
-        out.println(messageOut);
-        out.flush();
     }
-}
